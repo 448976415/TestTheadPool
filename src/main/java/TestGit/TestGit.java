5c99115471297cc7138b09296af9aaefa12c75cf
448976415 <448976415@qq.com>
2018-07-19 10:22:06 +0800

    idea commit

---
+++ src/main/java/TestGit/TestGit.java
@@ -0,0 +1,26 @@
+/**
+ * Copyright (C), 2015-2018, XXX有限公司
+ * FileName: TestGit
+ * Author:   sunhongyang
+ * Date:     2018/7/19 10:05
+ * Description: 测试上传
+ * History:
+ * <author>          <time>          <version>          <desc>
+ * 作者姓名           修改时间           版本号              描述
+ */
+package TestGit;
+
+/**
+ * 〈一句话功能简述〉<br> 
+ * 〈测试上传〉
+ *
+ * @author sunhongyang
+ * @create 2018/7/19
+ * @since 1.0.0
+ */
+public class TestGit {
+
+    public static void main(String[] args) {
+        System.out.println("args = [" + args + "]");
+    }
+}
